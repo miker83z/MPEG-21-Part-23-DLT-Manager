9281a19ca1e63e983c12b5e8f991934eb1ab1902
Mirko Zichichi <miker83z@proton.me>
2023-06-22 17:56:29 +0200

    fix storage

--- test/test-eth.js
+++ test/test-eth.js
@@ -1,6 +1,6 @@
 const {
   generateSmartContractSpecification,
-  OffChainStorage,
+  MockStorage,
   EthereumDeployer,
   EthereumParser,
 } = require('./..');
@@ -11,8 +11,7 @@ const generate = () => {
   return generateSmartContractSpecification(mco);
 };
 
-const deploy = async (smartContractSpecification) => {
-  const ipfs = new OffChainStorage();
+const deploy = async (smartContractSpecification, ipfs) => {
   const deployer = new EthereumDeployer(
     'http://127.0.0.1:8545',
     ipfs,
@@ -24,19 +23,19 @@ const deploy = async (smartContractSpecification) => {
   return res.options.address;
 };
 
-const parse = async (address) => {
-  const ipfs = new OffChainStorage();
+const parse = async (address, ipfs) => {
   const pars = new EthereumParser('http://127.0.0.1:8545', ipfs, address);
   return await pars.parseSmartContract();
 };
 
 const main = async () => {
+  const ipfs = new MockStorage();
   const smartContractSpecification = generate();
-  const address = await deploy(smartContractSpecification);
+  const address = await deploy(smartContractSpecification, ipfs);
   //const address = '0x9e0B0f5E7DF524cbe3BfbfAB3E733F1a7232ccB9';
   await sleep(2000);
   console.log('Parse');
-  const res = await parse(address);
+  const res = await parse(address, ipfs);
   console.log(res);
 };
 
